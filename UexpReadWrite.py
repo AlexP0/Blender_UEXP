580195e2f6e6ac2edf83fdc20dccc724c5d2e62f
AlexP0 <63766102+AlexP0@users.noreply.github.com>
2020-04-19 16:54:49 -0400

    UI buttons and LOD integration
    -can now find and persistently store LOD vertex data offsets
    -Import now optimized
    -Export button is out of service for now

--- UexpReadWrite.py
+++ UexpReadWrite.py
@@ -16,29 +16,45 @@ import struct
 Uexp = "A:\\Fallen Order Modding\\ModCalFace\\SwGame\\Content\\Characters\\Hero\\Rig\\Face\\hero_rig_face.uexp"
 Size = os.path.getsize(Uexp)
 
-#Vertex Data can be of any LOD, as long as it's a block of floats.
-#The beginning offset of the vertex data we want to edit
-VertexBegin = 6061849
-#This indicates the offset of the last vertex. 
-#So after this offset you'd still have 12 bytes to give us the coords of the last vertex.
-VertexEnd = 6471265
-
 #Not implemented.
 FaceBegin = 0 
 FaceEnd = 0 
 
-#####################################################################
-'''only relevant functions so far are CreateMesh() and WriteMesh()'''
-#####################################################################
-
+class UEXPSettings(bpy.types.PropertyGroup):
+    LOD0vStart: bpy.props.IntProperty()
+    LOD0vEnd: bpy.props.IntProperty()
+    LOD1vStart: bpy.props.IntProperty()
+    LOD1vEnd: bpy.props.IntProperty()
+    LOD2vStart: bpy.props.IntProperty()
+    LOD2vEnd: bpy.props.IntProperty()
+    LOD3vStart: bpy.props.IntProperty()
+    LOD3vEnd : bpy.props.IntProperty()
 
 #Read vertex data from uexp and create vertex cloud of it.
-def CreateMesh():
+def CreateMesh(LOD):
+    UEXPEditor = bpy.data.scenes['Scene'].UEXPEditor
+    if LOD == 0:
+        LODvStart = UEXPEditor.LOD0vStart
+        LODvEnd = UEXPEditor.LOD0vEnd
+        LODsuffix="_LOD0"
+    if LOD == 1:
+        LODvStart = UEXPEditor.LOD1vStart
+        LODvEnd = UEXPEditor.LOD1vEnd
+        LODsuffix="_LOD1"
+    if LOD == 2:
+        LODvStart = UEXPEditor.LOD2vStart
+        LODvEnd = UEXPEditor.LOD2vEnd
+        LODsuffix="_LOD2"
+    if LOD == 3:
+        LODvStart = UEXPEditor.LOD3vStart
+        LODvEnd = UEXPEditor.LOD3vEnd
+        LODsuffix="_LOD3"
     
-    #Read 4 bytes and unpack a float out of it for X, 
-    #read the next 4 for Y and the next for Z
-    def ReadVertex(rOffset):
-        with open (Uexp, 'rb') as f:
+    
+    with open (Uexp, 'rb') as f:
+        #Read 4 bytes and unpack a float out of it for X, 
+        #read the next 4 for Y and the next for Z
+        def ReadVertex(rOffset):
             f.seek(rOffset)
             
             bData = f.read(4)
@@ -51,20 +67,16 @@ def CreateMesh():
             v = [x,y,z]
             return v
 
-    vIndex = -1
-    vList = []
+        vIndex = -1
+        vList = []
 
-    #For each vertex we ask ReadVertex() to give us the coordinates.
-    #We move by steps of 12 bytes (3 floats)
-    for n in range(VertexBegin,VertexEnd,12):
-        vIndex += 1
-        v = ReadVertex(n)
-        
-        #We add the vertex read to the vList
-        vList.append(v)
-        #print(vIndex, " = ", v)
-
-    #print(vList)
+        #For each vertex we ask ReadVertex() to give us the coordinates.
+        #We move by steps of 12 bytes (3 floats)
+        for n in range(LODvStart,LODvEnd-12,12):
+            vIndex += 1
+            v = ReadVertex(n)
+            #We add the vertex read to the vList
+            vList.append(v)
 
     #Create vertex cloud
     def VCloud(object_name, coords, edges=[],faces=[]):
@@ -77,15 +89,14 @@ def CreateMesh():
         #This is where we construct the mesh by supplying vertex coords, edges and faces
         #For now only coords is implemented.
         mesh.from_pydata(coords, edges, faces)
-        
-        
+              
         object.show_name = True
         
         #gotta update to confirm mesh creation
         mesh.update()
         return object
 
-    vCloud = VCloud("Venator", vList)
+    vCloud = VCloud("AT-ST"+LODsuffix, vList)
     
     #We add the created object to a collection
     bpy.context.collection.objects.link(vCloud)
@@ -96,46 +107,45 @@ def CreateMesh():
 
 #This is where we write the modified vertex data back into the uexp
 def WriteMesh():
+    return
     
-    #Gotta have the object as active selected for this to work
-    CreatedMesh = bpy.context.active_object.data
+#    #Gotta have the object as active selected for this to work
+#    CreatedMesh = bpy.context.active_object.data
 
-    #Returns the binary coordinates of the given vertex index. 
-    def GetVCoords(index):
-        
-        #get coordinates of vertex
-        x = CreatedMesh.vertices[index].co[0]
-        y = CreatedMesh.vertices[index].co[1]
-        z = CreatedMesh.vertices[index].co[2]
-        
-        #turn these float coordinates into bytes
-        xb = struct.pack('<f',x)
-        yb = struct.pack('<f',y)
-        zb = struct.pack('<f',z)
-        
-        #binary vertex coordinates
-        vb = [xb,yb,zb]
-        
-        #print (xb,yb,zb)
-        return vb
-    
-    #Write the new binary vertex coordinates to the uexp
-    def WriteVBin(vOffset,vBinCoords):
-        with open (Uexp, 'rb+') as f:
-            f.seek(vOffset)
-            for i in vBinCoords:
-                f.write(i)
-        
-        
-    vIndex = -1
-    #For each vertex we get its coordinates and write them to uexp.
-    for n in range(VertexBegin, VertexEnd, 12):
-        vIndex += 1
-        vbCoords = GetVCoords(vIndex)
-        WriteVBin(n,vbCoords)
+#    #Returns the binary coordinates of the given vertex index. 
+#    def GetVCoords(index):
+#        
+#        #get coordinates of vertex
+#        x = CreatedMesh.vertices[index].co[0]
+#        y = CreatedMesh.vertices[index].co[1]
+#        z = CreatedMesh.vertices[index].co[2]
+#        
+#        #turn these float coordinates into bytes
+#        xb = struct.pack('<f',x)
+#        yb = struct.pack('<f',y)
+#        zb = struct.pack('<f',z)
+#        
+#        #binary vertex coordinates
+#        vb = [xb,yb,zb]
+#        
+#        #print (xb,yb,zb)
+#        return vb
+#    
+#    #Write the new binary vertex coordinates to the uexp
+#    def WriteVBin(vOffset,vBinCoords):
+#        with open (Uexp, 'rb+') as f:
+#            f.seek(vOffset)
+#            for i in vBinCoords:
+#                f.write(i)
+#        
+#        
+#    vIndex = -1
+#    #For each vertex we get its coordinates and write them to uexp.
+#    for n in range(VertexBegin, VertexEnd, 12):
+#        vIndex += 1
+#        vbCoords = GetVCoords(vIndex)
+#        WriteVBin(n,vbCoords)
 
-
-        
 
 #Finds start and end offsets of vertex data
 def FindVertexOffsets(SearchOffset):
@@ -179,10 +189,7 @@ def FindVertexOffsets(SearchOffset):
     print(startOffset, endOffset)            
     return startOffset, endOffset
 
-LOD0vStart,LOD0vEnd = FindVertexOffsets(0)
-LOD1vStart,LOD1vEnd = FindVertexOffsets(LOD0vStart)
-LOD2vStart,LOD2vEnd = FindVertexOffsets(LOD1vStart)
-LOD3vStart,LOD3vEnd = FindVertexOffsets(LOD2vStart)
+
 
 
 #This was an attempt at finding face data, did not work at all. 
@@ -193,140 +200,199 @@ LOD3vStart,LOD3vEnd = FindVertexOffsets(LOD2vStart)
 #even when trying with different index order.
 
 #We want to find where face data starts and end in the uexp file
-def FindFaceOffsets():
-    
-    startOffset = 0
-    endOffset = 0
-    
-    #From the selected object (an imported psk) we get the vertices of polygon[0] 
-    #and the last polygon
-    def FindFirstLastFaceVertices():
-        
-        polygons = bpy.context.active_object.data.polygons
-        lastFace = len(polygons)-1
-        
-        vs1 = polygons[0].vertices[0]
-        vs2 = polygons[0].vertices[1]
-        vs3 = polygons[0].vertices[2]
-        fStart = [vs1,vs2,vs3]
-        
-        ve1 = polygons[lastFace].vertices[0]
-        ve2 = polygons[lastFace].vertices[1]
-        ve3 = polygons[lastFace].vertices[2]
-        fEnd = [ve1,ve2,ve3]
-        
-        print(fStart, "...", fEnd)
-        
-        return fStart,fEnd
-    
-    fStart,fEnd = FindFirstLastFaceVertices()
-    
-    #read the entire file by block of 3 for each offset 
-    #and see if we match our vertice indices.
-    def SearchForIndices(offset,vGroup):
-        with open (Uexp, 'rb') as f:
-            
-            intTriplet=[]
-            
-            f.seek(offset)
-            
-            #3 times we will read 2 bytes to unpack as a short (integer) 
-            for n in range(3):
-                bData = f.read(2)
-                if len(bData)==2:
-                    iData = struct.unpack('<H',bData)                        
-                    intTriplet.append(iData[0])
-           
-            #print(intTriplet, "     ", vGroup)
-            if intTriplet == vGroup:                
-                return offset
-            else:
-                return 0
-        
-        
-    
-    #This controls the SearchForIndices function, gives it the needed offset. 
-    for n in range (Size):
-        #Here we supply the indices of the FIRST face
-        startOffset = SearchForIndices(n,fStart)
-        #Break out of the loop if we find the offset
-        if startOffset != 0:
-            print(startOffset)
-            break
+#def FindFaceOffsets():
+#    
+#    startOffset = 0
+#    endOffset = 0
+#    
+#    #From the selected object (an imported psk) we get the vertices of polygon[0] 
+#    #and the last polygon
+#    def FindFirstLastFaceVertices():
+#        
+#        polygons = bpy.context.active_object.data.polygons
+#        lastFace = len(polygons)-1
+#        
+#        vs1 = polygons[0].vertices[0]
+#        vs2 = polygons[0].vertices[1]
+#        vs3 = polygons[0].vertices[2]
+#        fStart = [vs1,vs2,vs3]
+#        
+#        ve1 = polygons[lastFace].vertices[0]
+#        ve2 = polygons[lastFace].vertices[1]
+#        ve3 = polygons[lastFace].vertices[2]
+#        fEnd = [ve1,ve2,ve3]
+#        
+#        print(fStart, "...", fEnd)
+#        
+#        return fStart,fEnd
+#    
+#    fStart,fEnd = FindFirstLastFaceVertices()
+#    
+#    #read the entire file by block of 3 for each offset 
+#    #and see if we match our vertice indices.
+#    def SearchForIndices(offset,vGroup):
+#        with open (Uexp, 'rb') as f:
+#            
+#            intTriplet=[]
+#            
+#            f.seek(offset)
+#            
+#            #3 times we will read 2 bytes to unpack as a short (integer) 
+#            for n in range(3):
+#                bData = f.read(2)
+#                if len(bData)==2:
+#                    iData = struct.unpack('<H',bData)                        
+#                    intTriplet.append(iData[0])
+#           
+#            #print(intTriplet, "     ", vGroup)
+#            if intTriplet == vGroup:                
+#                return offset
+#            else:
+#                return 0
+#        
+#        
+#    
+#    #This controls the SearchForIndices function, gives it the needed offset. 
+#    for n in range (Size):
+#        #Here we supply the indices of the FIRST face
+#        startOffset = SearchForIndices(n,fStart)
+#        #Break out of the loop if we find the offset
+#        if startOffset != 0:
+#            print(startOffset)
+#            break
 
-    #We do it all again to find the offset of the last face    
-    for n in range (Size):
-        endOffset = SearchForIndices(n,fEnd)
-        if endOffset != 0:
-            print(endOffset)
-            break
+#    #We do it all again to find the offset of the last face    
+#    for n in range (Size):
+#        endOffset = SearchForIndices(n,fEnd)
+#        if endOffset != 0:
+#            print(endOffset)
+#            break
         
+      
         
+def SearchLODOffsets(LOD=0):
+    UEXPEditor = bpy.data.scenes['Scene'].UEXPEditor
     
+    if LOD >= 0:
+        UEXPEditor.LOD0vStart,UEXPEditor.LOD0vEnd = FindVertexOffsets(0)
+    if LOD >= 1:
+        UEXPEditor.LOD1vStart,UEXPEditor.LOD1vEnd = FindVertexOffsets(UEXPEditor.LOD0vStart)
+    if LOD >= 2:
+        UEXPEditor.LOD2vStart,UEXPEditor.LOD2vEnd = FindVertexOffsets(UEXPEditor.LOD1vStart)
+    if LOD >= 3:
+        UEXPEditor.LOD3vStart,UEXPEditor.LOD3vEnd = FindVertexOffsets(UEXPEditor.LOD2vStart)
+    
+    return {'FINISHED'}
+
 
 
 
 ##### PANEL UI STUFF #######
 
-class EditUexp(bpy.types.Operator):
-    """Tooltip"""
-    bl_idname = "object.edit_uexp"
-    bl_label = "Import uexp"
-
-    @classmethod
-    def poll(cls, context):
-        return context.active_object is not None
-
+class ImportUexp(bpy.types.Operator):
+    """Creates a mesh from vertex data"""
+    bl_idname = "object.import_uexp"
+    bl_label = "Import"
+    
+    LODtoLoad: bpy.props.IntProperty(
+        name = 'LOD',
+        default = 0
+        )
+    
     def execute(self, context):
-        CreateMesh()
+        
+        CreateMesh(self.LODtoLoad)
         return {'FINISHED'}
+
     
 class ExportUexp(bpy.types.Operator):
-    """Tooltip"""
+    """Writes the modified vertex positions backs into the uexp"""
     bl_idname = "object.export_uexp"
-    bl_label = "Export uexp"
+    bl_label = "Export"
 
-    @classmethod
-    def poll(cls, context):
-        return context.active_object is not None
-
+    LODtoWrite: bpy.props.IntProperty(
+        name = 'LOD',
+        default = 0
+        )
+        
     def execute(self, context):
         WriteMesh()
         return {'FINISHED'}
 
-
-
+class SearchForOffsets(bpy.types.Operator):
+    """Searches the uexp for the begin and end offsets of vertex data"""
+    bl_idname = "object.search_offsets"
+    bl_label = "Search Vertex Offset"
+       
+    def execute(self, context):
+        SearchLODOffsets(2)
+        
+        return{'FINISHED'}
+    
 
 class UexpPanel(bpy.types.Panel):
-    """Creates a Panel in the Object properties window"""
+    """Creates a Panel in the Scene properties window"""
     bl_label = "Uexp Panel"
     bl_idname = "OBJECT_PT_uexp"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
-    bl_context = "data"
+    bl_context = "scene"
 
     def draw(self, context):
         layout = self.layout
-
+        UEXPEditor = bpy.data.scenes['Scene'].UEXPEditor
         obj = context.object
 
-        #row = layout.row()
-        #row.label(text="Load uexp", icon='CONSTRAINT_BONE')
         row = layout.row()
-        row.operator("object.edit_uexp", icon='SCRIPTPLUGINS')
+        row.operator("object.search_offsets", icon='ZOOM_ALL')
+        
         row = layout.row()
-        row.operator("object.export_uexp", icon='SCRIPTPLUGINS')
+        if UEXPEditor.LOD0vStart != 0:
+            row.label(text="LOD0: "+str(UEXPEditor.LOD0vStart)+" - "+str(UEXPEditor.LOD0vEnd), icon='MESH_ICOSPHERE')
+            LOD0Button = row.operator("object.import_uexp", icon='MESH_ICOSPHERE')
+            LOD0Button.LODtoLoad = 0
+            LOD0ButtonE = row.operator("object.export_uexp", icon='SCRIPTPLUGINS')
+            LOD0ButtonE.LODtoWrite = 0
+        
+        row = layout.row()
+        if UEXPEditor.LOD1vStart != 0:
+            row.label(text="LOD1: "+str(UEXPEditor.LOD1vStart)+" - "+str(UEXPEditor.LOD1vEnd), icon='MESH_ICOSPHERE')
+            LOD1Button = row.operator("object.import_uexp", icon='MESH_ICOSPHERE')
+            LOD1Button.LODtoLoad = 1
+            LOD1ButtonE = row.operator("object.export_uexp", icon='SCRIPTPLUGINS')
+            LOD1ButtonE.LODtoWrite = 1
+        
+        row = layout.row()
+        if UEXPEditor.LOD2vStart != 0:
+            row.label(text="LOD2: "+str(UEXPEditor.LOD2vStart)+" - "+str(UEXPEditor.LOD2vEnd), icon='MESH_ICOSPHERE')
+            LOD2Button = row.operator("object.import_uexp", icon='MESH_ICOSPHERE')
+            LOD2Button.LODtoLoad = 2
+            LOD2ButtonE = row.operator("object.export_uexp", icon='SCRIPTPLUGINS')
+            LOD2ButtonE.LODtoWrite = 2
+       
+        row = layout.row()
+        if UEXPEditor.LOD3vStart != 0:
+            row.label(text="LOD3: "+str(UEXPEditor.LOD3vStart)+" - "+str(UEXPEditor.LOD3vEnd), icon='MESH_ICOSPHERE')
+            LOD3Button = row.operator("object.import_uexp", icon='MESH_ICOSPHERE')
+            LOD3Button.LODtoLoad = 3
+            LOD3ButtonE = row.operator("object.export_uexp", icon='SCRIPTPLUGINS')
+            LOD3ButtonE.LODtoWrite = 3
 
+    
 
 def register():
     bpy.utils.register_class(UexpPanel)
-    bpy.utils.register_class(EditUexp)
+    bpy.utils.register_class(ImportUexp)
     bpy.utils.register_class(ExportUexp)
+    bpy.utils.register_class(SearchForOffsets)
+    bpy.utils.register_class(UEXPSettings)
+    bpy.types.Scene.UEXPEditor = bpy.props.PointerProperty(type=UEXPSettings)
 
 def unregister():
     bpy.utils.unregister_class(UexpPanel)
-    bpy.utils.unregister_class(EditUexp)
+    bpy.utils.unregister_class(ImportUexp)
     bpy.utils.unregister_class(ExportUexp)
+    bpy.utils.unregister_class(SearchForOffsets)
 
 if __name__ == "__main__":
     register()
